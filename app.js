c49c59d368083e0638149c1d2d94b01b8966cd80
patilarya76 <patilaryaanil07@gmail.com>
2023-11-29 14:01:57 +0530

    added successfully

---
+++ app.js
@@ -0,0 +1,23 @@
+// const observer = new IntersectionObserver((entries)=>{
+//   entries.forEach((entry)=>{
+//     console.log(entry)
+//     entry.target.classList.add('show');
+
+//   } else{
+//     entry.target.classList.remove('show')
+//   }
+  
+  
+  
+  
+//   )}
+
+
+
+
+
+// )
+
+
+// const hiddenElements = document.querySelectorAll('.hidden')
+// hiddenElements.forEach((el)=> observer.observe(el));
